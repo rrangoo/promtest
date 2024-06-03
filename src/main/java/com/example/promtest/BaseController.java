4d15d89d50e36747105e93935cd5c364f9f66275
kotelnikovdan <danila.kotelnikov@vk.team>
2024-06-03 22:08:11 +0300

    edit controller

--- src/main/java/com/example/promtest/BaseController.java
+++ src/main/java/com/example/promtest/BaseController.java
@@ -1,23 +1,15 @@
 package com.example.promtest;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Bazhov N.S.
  */
-@RestController
+@RestController("foo")
 public class BaseController {
     @GetMapping
     public String endpoint(){
         return "hello world";
-    }
-
-    @PostMapping
-    public String alert(@RequestBody String message){
-        System.out.println(message);
-        return "";
     }
 }
